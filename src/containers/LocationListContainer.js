d082637c1eee4f75b42d1ad4bae21acc61eedaa2
victor hugo <victor03016@gmail.com>
2019-09-07 18:52:31 +0200

    utilizacion de bindActionsCreators

--- src/containers/LocationListContainer.js
+++ src/containers/LocationListContainer.js
@@ -2,20 +2,21 @@ import React, { Component } from "react";
 import { bindActionCreators } from 'redux';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { setSelectedCity, setWeather } from "./../actions";
+// asterisco para indicar que importamos todo lo que esta en actions 
+import * as actions from "./../actions";
 import LocationList from "./../components/LocationList";
 import { getWeatherCities, getCity } from "./../reducers";
 
 class LocationListContainer extends Component {
     componentDidMount() {
-        const { setWeather, setCity, cities, city } = this.props;
+        const { setWeather, setSelectedCity, cities, city } = this.props;
         setWeather(cities);
-        setCity(city);
+        setSelectedCity(city);
     }
 
     handleSelectedLocation = city => {
         console.log("handleSelectedLocation");
-        this.props.setCity(city);
+        this.props.setSelectedCity(city);
     };
 
     render() {
@@ -29,17 +30,24 @@ class LocationListContainer extends Component {
 }
 
 LocationListContainer.propTypes = {
-    setCity: PropTypes.func.isRequired,
+    setSelectedCity: PropTypes.func.isRequired,
+    setWeather: PropTypes.func.isRequired,
     cities: PropTypes.array.isRequired,
     city: PropTypes.string.isRequired,
     citiesWeather: PropTypes.array
 };
 
+
+/* utilizacion de bindActionCreators permite escribir menos codigo, 
+tiene el mismo comportamiento que el siguiente código */
+const mapDispatchToPropsActions = dispatch => bindActionCreators(actions, dispatch);
+
+/*
 const mapDispatchToPropsActions = dispatch => ({
     setCity: value => dispatch(setSelectedCity(value)),
     setWeather: cities => dispatch(setWeather(cities))
 });
-
+*/
 const mapStateToProps = state => ({
     citiesWeather: getWeatherCities(state),
     city: getCity(state)
